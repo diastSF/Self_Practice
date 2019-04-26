a8bcc7c89221206f5a5e52be3bd0a32fec6eafe0
diast <diast.sf@gmail.com>
2019-04-26 19:17:54 +0700

    26-4-2019

--- Analysis_Visualization/dashboard_pokemon.py
+++ Analysis_Visualization/dashboard_pokemon.py
@@ -154,9 +154,94 @@ app.layout = html.Div([
                         id = 'catplotgraph',
                     )
         ]),
-        dcc.Tab(label='Scatter Plot', value='tab-3'),
-        dcc.Tab(label='Pie Chart', value='tab-4'),
-        dcc.Tab(label='Histogram', value='tab-5'),
+        dcc.Tab(label='Scatter Plot', value='tab-3', children = [
+            html.Div([
+                html.Div([
+                    html.P('Hue :'),
+                    dcc.Dropdown(
+                        id='huescatplot',
+                        options=[{'label':i,'value':i} for i in ['Legendary','Generation','Type 1','Type 2']],
+                        value='Legendary'
+                    )
+                ], className = 'col-4'),
+                html.Div([
+                    html.P('X :'),
+                    dcc.Dropdown(
+                        id='xscatplot',
+                        options=[{'label':i,'value':i} for i in dfPokemon.columns[4:11]],
+                        value='Total'
+                    )
+                ], className = 'col-4'),
+                html.Div([
+                    html.P('Y :'),
+                    dcc.Dropdown(
+                        id='yscatplot',
+                        options=[{'label':i,'value':i} for i in dfPokemon.columns[4:11]],
+                        value='Attack'
+                    )
+                ], className = 'col-4'),
+            ], className = 'row'),
+            html.Br(),
+            html.Br(),
+            html.Br(),
+            dcc.Graph(
+                id = 'scatplotgraph',
+            )
+        ]),
+        dcc.Tab(label='Pie Chart', value='tab-4', children = [
+            html.Div([
+                html.Div([
+                    html.P('Group :'),
+                    dcc.Dropdown(
+                        id='grouppieplot',
+                        options=[{'label':i,'value':i} for i in ['Legendary','Generation','Type 1','Type 2']],
+                        value='Legendary'
+                    )
+                ], className = 'col-4')
+            ], className = 'row'),
+            html.Br(),
+            html.Br(),
+            html.Br(),
+            dcc.Graph(
+                id = 'pieplotgraph',
+            )
+        ]),
+        dcc.Tab(label='Histogram', value='tab-5', children = [
+            html.Div([
+                html.Div([
+                    html.P('X :'),
+                    dcc.Dropdown(
+                        id='xhistplot',
+                        options=[{'label':i,'value':i} for i in dfPokemon.columns[4:11]],
+                        value='Total'
+                    )
+                ], className = 'col-4'),
+                html.Div([
+                    html.P('Hue :'),
+                    dcc.Dropdown(
+                        id='huehistplot',
+                        options=[{'label':i,'value':i} for i in ['All','Legendary','Generation']],
+                        value='All'
+                    )
+                ], className = 'col-4'),
+                html.Div([
+                    html.P('Std :'),
+                    dcc.Dropdown(
+                        id='stdhistplot',
+                        options=[{'label':'{} Standart Deviation'.format(i),'value':i} for i in ['1','2','3']],
+                        value='2'
+                    )
+                ], className = 'col-4')
+            ], className = 'row'),
+            html.Br(),
+            html.Br(),
+            html.Br(),
+            html.Br(),
+            html.Br(),
+            dcc.Graph(
+                id = 'histplotgraph',
+            )
+        ]),
     ], style={
         'fontFamily':'system-ui'
     }, content_style={
@@ -254,6 +339,162 @@ def disabled_stats(jenis):
     if jenis != 'Bar':
         return True
     return False
+
+#_____________________________CALLBACK SCATTER PLOT______________________________
+
+legendDict = {
+    'Legendary' : {'True' : 'Legendary',
+                    'False' : 'Non-Legendary'},
+    'Generation' : {1 : '1st Generation',
+                    2 : '2nd Generation',
+                    3 : '3rd Generation',
+                    4 : '4th Generation',
+                    5 : '5th Generation',
+                    6 : '6th Generation'},
+    'Type 1' : {i : i for i in dfPokemon['Type 1'].unique()},
+    'Type 2' : {i : i for i in dfPokemon['Type 2'].unique()}
+}
+
+@app.callback(Output('scatplotgraph','figure'),
+                [Input('huescatplot','value'),
+                Input('xscatplot','value'),
+                Input('yscatplot','value')]
+)
+
+def update_scatplot(hue,x,y):
+    return dict(
+        data = [
+            go.Scatter(
+                x = dfPokemon[dfPokemon[hue] == var][x],
+                y = dfPokemon[dfPokemon[hue] == var][y],
+                name = legendDict[hue][var],
+                mode = 'markers'
+            ) for var in dfPokemon[hue].unique()
+        ],
+        layout = dict(
+            title = 'Scatter Plot Pokemon',
+            xaxis = dict(title = x),
+            yaxis = dict(title = y),
+            margin = {'l':40,'b':40,'t':40,'r':10},
+            hovermode = 'closest'
+        )
+    )
+
+#_____________________________CALLBACK PIE PLOT______________________________
+
+@app.callback(Output('pieplotgraph','figure'),
+                [Input('grouppieplot','value')]
+)
+
+def update_pieplot(cat):
+    return dict(
+        data = [
+            go.Pie(
+                labels = [legendDict[cat][var] for var in dfPokemon[cat].unique()],
+                values = [dfPokemon[dfPokemon[cat] == var]['Total'].count()
+                            for var in dfPokemon[cat].unique()],
+                hoverinfo = 'label+value',
+                textinfo = 'percent'
+            )
+        ],
+        layout = go.Layout(
+            title = 'Pie Chart Pokemon',
+            margin={'l': 160, 'b': 40, 't': 40, 'r': 10}
+        )
+    )
+
+#_____________________________CALLBACK HISTOGRAM______________________________
+
+rowcoldict = {
+    'Legendary' : [1,2],
+    'Generation' : [3,2]
+}
+
+@app.callback(Output('histplotgraph','figure'),
+                [Input('xhistplot','value'),
+                Input('huehistplot','value'),
+                Input('stdhistplot','value')]
+)
+
+def update_histplot(x,hue,std):
+    if hue == 'All':
+        return dict(
+            data = [
+                go.Histogram(
+                    x = dfPokemon[(dfPokemon['Total'] >= (
+                        dfPokemon['Total'].mean() - (2 * dfPokemon['Total'].std())))
+                            & (dfPokemon['Total'] <= (
+                        dfPokemon['Total'].mean() + (2 * dfPokemon['Total'].std())))
+                        ]['Total'],
+                    name = 'Normal',
+                    marker = dict(color = '#83BC74')
+                ),
+                go.Histogram(
+                    x = dfPokemon[(dfPokemon['Total'] < (
+                        dfPokemon['Total'].mean() - (2 * dfPokemon['Total'].std())))
+                            | (dfPokemon['Total'] > (
+                        dfPokemon['Total'].mean() + (2 * dfPokemon['Total'].std())))
+                        ]['Total'],
+                    name = 'Outlier',
+                    marker = dict(color = '#E55261')
+                )
+            ],
+            layout = go.Layout(
+                title = 'Histogram {} Stats Pokemon'.format(x),
+                xaxis = dict( title = 'Total'),
+                yaxis = dict( title = 'Count'),
+                height = 600,
+                width = 1100
+            )
+        )
+
+
+
+    fig = tools.make_subplots(
+        rows = rowcoldict[hue][0], cols = rowcoldict[hue][1],
+    )
+
+    array = dfPokemon[hue].unique().reshape(rowcoldict[hue][0],rowcoldict[hue][1])
+
+    for row in range(1, rowcoldict[hue][0]+1):
+        for col in range(1,rowcoldict[hue][1]+1):
+            dfFilter = dfPokemon[dfPokemon[hue] == array[row-1,col-1]]
+            fig.append_trace(
+                go.Histogram(
+                    x = dfFilter[(dfFilter[x] >= (
+                        dfFilter[x].mean() - (int(std) * dfFilter[x].std())))
+                            & (dfFilter[x] <= (
+                        dfFilter[x].mean() + (int(std) * dfFilter[x].std())))
+                        ][x],
+                    name = 'Normal {}'.format(legendDict[hue][array[row-1,col-1]]),
+                    marker = dict(color = '#83BC74')
+                ),
+                row,col
+            ),
+            fig.append_trace(
+                go.Histogram(
+                    x = dfFilter[(dfFilter[x] < (
+                        dfFilter[x].mean() - (int(std) * dfFilter[x].std())))
+                            | (dfFilter[x] > (
+                        dfFilter[x].mean() + (int(std) * dfFilter[x].std())))
+                        ][x],
+                    name = 'Outlier {}'.format(legendDict[hue][array[row-1,col-1]]),
+                    marker = dict(color = '#E55261')
+                ),
+                row,col
+            )
+
+    for axis in range(1, len(dfPokemon[hue].unique())+1):
+        fig['layout']['xaxis{}'.format(axis)].update(title = x)
+        fig['layout']['yaxis{}'.format(axis)].update(title = 'Count')
+
+    if hue == 'Generation':
+        fig['layout'].update(height = 800, width = 1100, title = 'Histogram {} Stats Pokemon'.format(x))
+    if hue == 'Legendary':
+        fig['layout'].update(height = 500, width = 1100, title = 'Histogram {} Stats Pokemon'.format(x))
+    
+    return fig
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
